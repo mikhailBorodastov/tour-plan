cbf91de882a00ad0c8ab24769a391ae24f9b33e9
mikhailBorodastov <borodastoff@gmail.com>
2021-11-18 03:20:40 +0300

    create: added reviews block fixes with media

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-const swiper = new Swiper(".swiper", {
+const swiper = new Swiper(".hotel__swiper", {
   // Optional parameters
   direction: "horizontal",
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".swiper-button--next",
-    prevEl: ".swiper-button--prev",
+    nextEl: ".hotel__swiper-button--next",
+    prevEl: ".hotel__swiper-button--prev",
+  },
+  effect: "slide",
+});
+const reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".reviews-slider__button--next",
+    prevEl: ".reviews-slider__button--prev",
   },
   effect: "slide",
 });
